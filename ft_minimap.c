c08e4e2a388c8c82f379a3ce533f93067bf02ed2
De Los Muros Tom <nine@TomDlm-MacBook-Pro.local>
2023-09-04 14:13:29 +0200

    Vecteur init

--- ft_minimap.c
+++ ft_minimap.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 15:59:35 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/09/04 11:42:45 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/09/04 14:13:22 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,32 @@ void	ft_map(t_master *s_m, int x, int y, int c)
 		{
 			w = j + x * 16;
 			if (c == 1)
-				img_pix_put(&s_m->img, w, h, create_trgb(0, 134, 134, 134));
+				img_pix_put(&s_m->img, w, h, create_trgb(0, 107, 107, 107));
             else if (c == 2)
+				img_pix_put(&s_m->img, w, h, create_trgb(0, 0, 255, 0));
+			else if (c == 3)
 				img_pix_put(&s_m->img, w, h, create_trgb(0, 0, 255, 0));
 		}
 	}
+}
+
+void	ft_color_door(t_master *s_m, int x, int y, int c)
+{
+	int	x;
+	int	y;
+
+	if (s_m->minimap)
+	{
+		y * 16;
+		x * 16;
+	}
+	else
+	{
+		y * 8;
+		x * 8;
+	}
+	if (c == 3)
+		img_pix_put(&s_m->img, w, h, create_trgb(0, 255, 255, 255));
 }
 
 void	ft_draw_square(t_master *s_m, int x, int y, int c)
@@ -61,7 +82,7 @@ void	ft_draw_square(t_master *s_m, int x, int y, int c)
 			{
 				w = j + x * 8;
 				if (c == 1)
-					img_pix_put(&s_m->img, w, h, create_trgb(0, 134, 134, 134));
+					img_pix_put(&s_m->img, w, h, create_trgb(0, 107, 107, 107));
             	else if (c == 2)
 					img_pix_put(&s_m->img, w, h, create_trgb(0, 0, 255, 0));
 			}
@@ -84,6 +105,8 @@ void	ft_minimap(t_master *s_m, char **map)
 				ft_draw_square(s_m, x, y, 1);
 			else if (map[y][x] == 'P')
 				ft_draw_square(s_m, x, y, 2);
+			else if (map[y][x] == 'D')
+				ft_color_door(s_m, x, y);
 		}
 	}
 }
