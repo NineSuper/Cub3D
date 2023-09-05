90956eae4fd26f7ed27e62ff6a8ea96016f91661
De Los Muros Tom <nine@TomDlm-MacBook-Pro.local>
2023-09-05 20:33:12 +0200

    try it

--- ft_text.c
+++ ft_text.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 20:25:03 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/09/05 20:30:19 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/09/05 20:33:10 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_text_mac(t_master *s_m)
 {
-	mlx_string_put(s_m->mlx, s_m->win, 2, 222, 0XFFFFF, "HELP OPTION");
+	mlx_string_put(s_m->mlx, s_m->win, 50, 50, create_trgb(0, 0, 0, 255), "HELP OPTION");
 }
 
 void	ft_text(t_master *s_m)
