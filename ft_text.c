50c43ca00d0fc951712b33877d2de36720a62fe7
De Los Muros Tom <nine@TomDlm-MacBook-Pro.local>
2023-09-05 20:41:42 +0200

    try it

--- ft_text.c
+++ ft_text.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 20:25:03 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/09/05 20:40:21 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/09/05 20:41:39 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_text_mac(t_master *s_m)
 	mlx_string_put(s_m->mlx, s_m->win, 4, 242, 0xFFFFFF, "Extra Key");
 	mlx_string_put(s_m->mlx, s_m->win, 4, 262, 0xFFFFFF, "M: Map");
 	mlx_string_put(s_m->mlx, s_m->win, 4, 282, 0xFFFFFF, "ESC: Exit");
-	mlx_string_put(s_m->mlx, s_m->win, 4, 302, 0xFFFFFF, "SPACE: none");
+	mlx_string_put(s_m->mlx, s_m->win, 4, 302, 0xFFFFFF, "H: enable/disable option");
 }
 
 void	ft_text(t_master *s_m)
