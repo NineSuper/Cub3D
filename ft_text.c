faa5c9159b40619b49fa1ddf38d2fb3998622c10
De Los Muros Tom <nine@TomDlm-MacBook-Pro.local>
2023-09-05 20:39:26 +0200

    try it

--- ft_text.c
+++ ft_text.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 20:25:03 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/09/05 20:38:03 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/09/05 20:39:17 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	ft_text_mac(t_master *s_m)
 {
-	mlx_string_put(s_m->mlx, s_m->win, 4, 222, create_trgb(0, 0, 0, 255), "Extra Key");
-	mlx_string_put(s_m->mlx, s_m->win, 4, 242, create_trgb(0, 0, 0, 255), "M: Map");
-	mlx_string_put(s_m->mlx, s_m->win, 4, 262, create_trgb(0, 0, 0, 255), "ESC: Exit");
-	mlx_string_put(s_m->mlx, s_m->win, 4, 282, create_trgb(0, 0, 0, 255), "SPACE: Shoot");
+	mlx_string_put(s_m->mlx, s_m->win, 4, 222, 0xFFFFFF, "Extra Key");
+	mlx_string_put(s_m->mlx, s_m->win, 4, 242, 0xFFFFFF, "M: Map");
+	mlx_string_put(s_m->mlx, s_m->win, 4, 262, 0xFFFFFF, "ESC: Exit");
+	mlx_string_put(s_m->mlx, s_m->win, 4, 282, 0xFFFFFF, "SPACE: Shoot");
 }
 
 void	ft_text(t_master *s_m)
