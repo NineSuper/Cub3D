20408c5caedb801f19a704f23cbb936fa6219f4e
De Los Muros Tom <nine@TomDlm-MacBook-Pro.local>
2023-09-05 20:49:19 +0200

    try it

--- ft_text.c
+++ ft_text.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 20:25:03 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/09/05 20:48:23 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/09/05 20:49:15 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ void	ft_text(t_master *s_m)
 		ft_text_mac(s_m);
 	else
 	{
-		mlx_string_put(s_m->mlx, s_m->win, 4, 262, 0xFFFFFF, "M: Map");
-		mlx_string_put(s_m->mlx, s_m->win, 4, 282, 0xFFFFFF, "ESC: Exit");
-		mlx_string_put(s_m->mlx, s_m->win, 4, 302, 0xFFFFFF, "H: on/off help");
+		mlx_string_put(s_m->mlx, s_m->win, 4, 262 * 2, 0xFFFFFF, "M: Map");
+		mlx_string_put(s_m->mlx, s_m->win, 4, 282 * 2, 0xFFFFFF, "ESC: Exit");
+		mlx_string_put(s_m->mlx, s_m->win, 4, 302 * 2, 0xFFFFFF, "H: on/off help");
 	}
 }
