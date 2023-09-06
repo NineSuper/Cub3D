a629d009d25774c5848eacd0a96b501e01e22470
De Los Muros Tom <nine@TomDlm-MacBook-Pro.local>
2023-09-06 07:16:50 +0200

    fix utils.h

--- ft_utils.c
+++ ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 12:38:51 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/09/06 07:13:16 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/09/06 07:16:44 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ int	ft_msg(char *str, t_master *s_m)
 {
 	if (!ft_strncmp("NO", str, 2))
 	{
-		s_m->map.NO = ft_split_text(str, s_m);
-		ft_printf("%sNO: %s\033[00m\n", C_GREEN, s_m->map.NO);
+		s_m->map.no = ft_split_text(str, s_m);
+		ft_printf("%sNO: %s\033[00m\n", C_GREEN, s_m->map.no);
 	}
 	else if (!ft_strncmp("SO", str, 2))
 	{
-		s_m->map.SO = ft_split_text(str, s_m);
-		ft_printf("%sSO: %s\033[00m\n", C_GREEN, s_m->map.SO);
+		s_m->map.so = ft_split_text(str, s_m);
+		ft_printf("%sSO: %s\033[00m\n", C_GREEN, s_m->map.so);
 	}
 	else if (!ft_strncmp("WE", str, 2))
 	{
-		s_m->map.WE = ft_split_text(str, s_m);
-		ft_printf("%sWE: %s\033[00m\n", C_GREEN, s_m->map.WE);
+		s_m->map.we = ft_split_text(str, s_m);
+		ft_printf("%sWE: %s\033[00m\n", C_GREEN, s_m->map.we);
 	}
 	else if (!ft_strncmp("ea", str, 2))
 	{
