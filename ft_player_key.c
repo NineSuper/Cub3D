71a190fc3b4ca8a00cdc745523c93ff84d9cbb49
De Los Muros Tom <nine@TomDlm-MacBook-Pro.local>
2023-09-06 12:21:04 +0200

    player map_test

--- ft_player_key.c
+++ ft_player_key.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 17:24:20 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/09/06 12:19:46 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/09/06 12:21:00 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,6 @@ void	ft_key_player(int key, t_master *s_m)
 		s_m->minimap = ft_press(s_m->minimap);
 	if (key == 104)
 		s_m->help = ft_press(s_m->help);
-	if (key == 199)
-		s_m->map.map[3][3] = 'P';
+	if (key == 119)
+		s_m->map.map[2][2] = 'P';
 }
