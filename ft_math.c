d1cc1c6b6c6a55dd829cde19869814bb38327ba2
NineSuper <45767648+NineSuper@users.noreply.github.com>
2024-01-09 15:35:23 +0100

    Add files via upload

--- ft_math.c
+++ ft_math.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_math.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lumontgo <lumontgo@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:41:26 by tde-los-          #+#    #+#             */
-/*   Updated: 2024/01/09 15:14:19 by lumontgo         ###   ########.fr       */
+/*   Updated: 2024/01/09 15:34:47 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,8 +135,7 @@ void	ft_texture(t_master *s_m, t_draw draw, int x)
 	{
 		player->texy = (int)player->texpos & (s_m->so.h - 1);
 		player->texpos += player->step;
-		int color = (unsigned int)s_m->so.addr[s_m->so.h * player->texy + player->texx];
-		put_pixel_img(s_m->img, x, y, color);
+		put_pixel_img(s_m->img, x, y, get_pixel_img(s_m->so, player->texx, player->texy));
 		y++;
 	}
 }
@@ -149,8 +148,6 @@ void	ft_ray(t_master *s_m, t_player *player, char **map)
 
 	x = -1;
 	int	j = 0;
-	//ft_rplace(s_m, s_m->so, s_m->img, (t_coords){200, 50, 256, 256});
-	s_m->so = ft_img_resize(s_m, s_m->so, 16, 16);
 	while (++x < WIDTH)
 	{
 		ft_init_ray(player, x);
@@ -170,110 +167,3 @@ void	ft_ray(t_master *s_m, t_player *player, char **map)
  			ft_texture(s_m, (t_draw){drawstart, drawend}, x);
 	}
 }
-
-
-// void	ft_ray(t_master *s_m, t_player *player, char **map)
-// {
-// 	int	x = 0;
-
-// 	while (x < WIDTH)
-// 	{
-// 		if (!player->posx && !player->posy)
-// 		{
-// 			player->posx = player->x;
-// 			player->posy = player->y;
-// 		}	
-// 		player->camerax = 2 * x / (double)WIDTH  - 1;
-// 		player->raydirx = player->dirx + player->planex * player->camerax;
-// 		player->raydiry = player->diry + player->planey * player->camerax;
-
-// 		player->mapx = (int)player->posx;
-// 		player->mapy = (int)player->posy;
-
-// 		player->deltadistx = (1 / player->raydirx) * \
-// 			(1 - (2 * (1 / player->raydirx < 0)));
-// 		player->deltadisty = (1 / player->raydiry) * \
-// 			(1 - (2 * (1 / player->raydiry < 0)));
-		
-// 		if (player->raydirx < 0)
-// 		{
-// 			player->stepx = -1;
-// 			player->sidedistx = (player->posx - player->mapx) * player->deltadistx;
-// 		}
-// 		else
-// 		{
-// 			player->stepx = 1;
-// 			player->sidedistx = (player->mapx + 1.0 - player->posx) * player->deltadistx;
-// 		}
-// 		if (player->raydiry < 0)
-// 		{
-// 			player->stepy = -1;
-// 			player->sidedisty = (player->posy - player->mapy) * player->deltadisty;
-// 		}
-// 		else
-// 		{
-// 			player->stepy = 1;
-// 			player->sidedisty = (player->mapy + 1.0 - player->posy) * player->deltadisty;
-// 		}
-
-// 		while (player->hit == 0)
-// 		{
-// 			if (player->sidedistx < player->sidedisty)
-// 			{
-// 				player->sidedistx += player->deltadistx;
-// 				player->mapx += player->stepx;
-// 				player->side = 0;
-// 			}
-// 			else
-// 			{
-// 				player->sidedisty += player->deltadisty;
-// 				player->mapy += player->stepy;
-// 				player->side = 1;
-// 			}
-// 			if (map[player->mapy][player->mapx] == '1')
-// 				player->hit = 1;
-// 		}
-
-// 		if (player->side == 0)
-// 			player->perpwalldist = (player->sidedistx - player->deltadistx);
-// 		else
-// 			player->perpwalldist = (player->sidedisty - player->deltadisty);
-
-// 		player->lineheight = (int)(HEIGHT / player->perpwalldist);
-
-
-// 		int drawstart = -player->lineheight / 2 + HEIGHT / 2;
-// 		if (drawstart < 0)
-// 			drawstart = 0;
-// 		int drawend = player->lineheight / 2 + HEIGHT / 2;
-// 		if (drawend >= HEIGHT)
-// 			drawend = HEIGHT - 1;
-
-// 		if (player->side == 0)
-// 			player->wallx = player->posy + player->perpwalldist * player->raydiry;
-// 		else
-// 			player->wallx = player->posx + player->perpwalldist * player->raydirx;
-
-// 		player->wallx -= floor(player->wallx);
-
-// 		player->texx = (int)(player->wallx * (double)(s_m->so.w));
-// 		if (player->side == 0 && player->raydirx > 0)
-// 			player->texx = s_m->so.w - player->texx - 1;
-// 		if (player->side == 1 && player->raydiry < 0)
-// 			player->texx = s_m->so.w - player->texx - 1;
-		
-// 		player->step = 1.0 * s_m->so.h / player->lineheight;
-// 		player->texpos = (drawstart - HEIGHT / 2 + player->lineheight / 2) * player->step;
-
-// 		int y = drawstart;
-// 		while (y < drawend)
-// 		{
-// 			player->texy = (int)player->texpos & (s_m->so.h - 1);
-// 			player->texpos += player->step;
-// 			int color = (unsigned int)s_m->so.addr[s_m->so.h * player->texy + player->texx];
-// 			put_pixel_img(s_m->img, x, y, color);
-// 			y++;
-// 		}
-// 		x++;
-// 	}
-// }
